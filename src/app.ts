a9598cb4f17674dbb2c619d92c1d75e2e2fb7f3b
Diogo Nava <diogonsdf@gmail.com>
2022-02-15 11:58:29 -0300

    feat: added index for routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import "reflect-metadata";
+import { initializerRouter } from "./routes";
 
 const app = express();
+
+initializerRouter(app);
 
 app.use(express.json());
 
