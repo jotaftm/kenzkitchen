39f13b1d4964fffaa875614ee6ba777de9389135
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-02-15 13:41:31 -0300

    feat: create generic error class

---
+++ src/errors/errorHandler.error.ts
@@ -0,0 +1,9 @@
+export class ErrorHandler {
+  public readonly message: string;
+  public readonly statusCode: number;
+
+  constructor(message: any, statusCode: number) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
